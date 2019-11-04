9b545b444e8fe1ea30d65bbe26d821471f050383
bemal_dvanitha <47688435+bemaldvanitha@users.noreply.github.com>
2019-11-04 21:12:18 +0530

    update 1.1

--- src/main.java
+++ src/main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class main {
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
-        ArrayList <student> studentArrayList=new ArrayList<>();
-        ArrayList <lecture> lectureArrayList=new ArrayList<>();
+        ArrayList <students> studentArrayList=new ArrayList<>();
+        ArrayList <lectures> lectureArrayList=new ArrayList<>();
 
-        int a=1,b,c,d,e;
-        String k,l,m,n,o,p;
-        while(a==1){
+        int a=1,b,c,d,e=196,l;
+        String k,m,n,o,p;
+        while(e==196){
             System.out.println("Welcome to univercity managemtn system");
             System.out.println("plese select an opption");
             System.out.println("enter 1 to register as student enter 2 to register as lecturer enter 3 to exit");
@@ -18,25 +18,26 @@ public class main {
                 System.out.println("enter name");
                 k=scan.next();
                 System.out.println("enter age");
-                l=scan.next();
-                student stu=new student(k,l);
+                l=scan.nextInt();
+                students stu=new students(k,l);
                 stu.showCourses();
                 System.out.println("enter course id ");
                 m=scan.next();
                 stu.selectCouse(m);
-
+                
                 stu.show();
             }
             else if(a==2){
                 System.out.println("enter name");
                 k=scan.next();
                 System.out.println("enter age");
-                l=scan.next();
-                lecture lec=new lecture(k,l);
+                l=scan.nextInt();
+                lectures lec=new lectures(k,l);
+                System.out.println("Registrer completed");
                 lec.show();
             }
             else if(a==3){
-                a=2;
+                e=0;
             }
             else{
                 System.out.println("wrong command");
@@ -46,14 +47,34 @@ public class main {
     }
 }
 
-class student extends couses{
+class students extends couses{
     private String name;
     private int age;
     private ArrayList <String> selectCourses=new ArrayList<String>();
 
-    public student(name,age){
+    public students() {
+    	
+    }
+    
+    public students(String name,int age){
         this.name=name;
         this.age=age;
+    }
+    
+    public void setName(String name) {
+    	this.name=name;
+    }
+    
+    public void setAge(int age) {
+    	this.age=age;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public int getAge() {
+    	return age;
     }
 
     public void selectCouse(String id){
@@ -61,11 +82,11 @@ class student extends couses{
     }
 
     public void show(){
+    	System.out.println("register completed");
         System.out.println("Name : "+name);
         System.out.println("Age : "+age);
-        for(int a=0;a<selectCourses.size();a++){
-            System.out.println("Subject : "+selectCourses.get(a)" ");
-        }
+        System.out.println("Course "+courseId.get(Integer.parseInt(selectCourses.get(0)))+"   "+courseName.get(Integer.parseInt(selectCourses.get(0))));
+        
     }
 
 }
@@ -93,13 +114,33 @@ class couses{
     }
 }
 
-class lecture extends couses{
+class lectures extends couses{
     private String name;
     private int age;
+    
+    public lectures() {
+    	
+    }
 
-    public lecture(name,age){
+    public lectures(String name,int age){
         this.name=name;
         this.age=age;
+    }
+    
+    public void setName(String name) {
+    	this.name=name;
+    }
+    
+    public void setAge(int age) {
+    	this.age=age;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public int getAge() {
+    	return age;
     }
 
     public void addCourses(String id,String name){
@@ -113,4 +154,5 @@ class lecture extends couses{
 
     }
 }
+
 
